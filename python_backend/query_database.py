7eae451fde6b61448b7c5d04d00167d3b9cb11f7
marcel <marcelaboumatar@gmail.com>
2025-05-03 05:08:52 -0400

    setup most of the database

---
+++ python_backend/query_database.py
@@ -0,0 +1,58 @@
+#for git
+from dotenv import load_dotenv
+import os
+#pymango
+import pymongo
+
+
+def query_database(query_text:str, database_name:str, collection_name:str):
+
+  load_dotenv()
+
+  DATABASE_LOGIN = os.getenv("DATABASE_LOGIN") #DATABASE_LOGIN is in the form username:password
+
+  uri = f"mongodb+srv://{DATABASE_LOGIN}@gdsc2025.cn3wt5n.mongodb.net/?retryWrites=true&w=majority&appName=GDSC2025"
+
+  # connect to your Atlas cluster
+  client = pymongo.MongoClient(uri)
+
+  #TODO
+  #generate this vector using the query_text
+  query_vector = []
+
+  #these are the current info i got but add more if there is more data
+  #By-law No.
+  #By-law Title
+
+  # define pipeline
+  pipeline = [
+    {
+      '$vectorSearch': {
+        'index': 'vector_index', 
+        'path': 'plot_embedding', 
+        'queryVector': query_vector,
+        'numCandidates': 150, 
+        'limit': 10
+      }
+    }, {
+      '$project': {
+        '_id': 0, 
+        'By-law No.': 1, 
+        'By-law Title': 1, 
+        'score': {
+          '$meta': 'vectorSearchScore'
+        }
+      }
+    }
+  ]
+
+  #TODO optional: loop this for multiple collections if that is how to data ends up
+  # run pipeline
+  result = client[database_name][collection_name].aggregate(pipeline)
+
+  # print results
+  for i in result:
+      print(i)
+  
+  return result 
+  
